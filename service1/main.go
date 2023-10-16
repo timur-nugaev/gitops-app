d5903680111543a251c7f970e6ffa83056763ce4
Timur Nugaev <nugaev.t@gmail.com>
2023-10-16 20:13:17 +0300

    service folder created

---
+++ service1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Processing request...")
+		w.Write([]byte("Hello!"))
+	})
+	fmt.Println("Starting server at 8080...")
+	fmt.Println("hello!")
+	http.ListenAndServe(":8080", nil)
+}
